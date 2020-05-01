21d7fb6353fabc920ad2181843bbe4e47824ebaf
Ryan <ryanmagill97@gmail.com>
2020-04-30 19:46:56 -0500

    added src folder and app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+
+import './App.css';
+
+
+function App() {
+  // Declare a new state variable to set all employees
+  const [employees, setEmployees] = useState([]);
+
+  // Declare a new state to filter 
+  const [filterText, setFilterText] = useState('');
+  const filteredEmployees = employees.filter((e) => {
+    // By first name
+    if(e.name.first.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+     // By lame name
+    else if(e.name.last.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+     // By email
+    else if(e.email.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+    // By Cell Number
+    else if(e.cell.includes(filterText) ) {
+      return true;
+    }
+    else {
+      return false
+    }
+  })
+
+  
+
+  useEffect(()=> {  
+    // Make a request for a user with a given ID
+    axios.get('https://randomuser.me/api/?results=50&nat=us')
+.then(function (response) {
+  // handle success
+  setEmployees(response.data.results)
+})
+.catch(function (error) {
+  // handle error
+})
+.then(function () {
+  // always executed
+});
+  },[]) 
+
+
+
+  return (
+    <div className="App">
+      <Header />
+      <Search setFilterText={setFilterText} />
+      <Table employees={filteredEmployees} />
+    </div>
+  );
+}
+
+export default App;
